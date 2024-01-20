8d629bb222a819be73fc5ca2312bfdb9c0cb4587
prajwal952 <73274063+prajwal952@users.noreply.github.com>
2024-01-20 11:28:00 +0530

    adding index

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("connected");
+
+function redirectToProject1(){
+  window.location.href="1-BgColorChanger/index.html";
+}
+
+function redirectToProject2(){
+  window.location.href="2-BMICalculator/index.html";
+}
+
+function redirectToProject3(){
+  window.location.href="3-localTime/index.html";
+}
+function redirectToProject4(){
+  window.location.href="4-GuessTheNumber/index.html";
+}
